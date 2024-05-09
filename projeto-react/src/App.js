2d48f68cc3ad7e09758bc7559e823904c25b5658
elucasrg <lrggbi@gmail.com>
2024-05-09 15:41:35 -0300

    projeto03

--- projeto-react/src/App.js
+++ projeto-react/src/App.js
@@ -1,11 +1,39 @@
+import { BrowserRouter as Router, Switch, Route, link } from 'react-router-dom'
+import Sobre from './components/pages/Sobre'
+import Skills from './components/pages/skills'
+import Projetos from './components/pages/Projetos'
+import Contatos from './components/pages/Contatos'
 
-
+import Container from './components/pages/layout/Container'
 
 function App() {
   return (
-    <div className='App'>
-      <p>Lucas Guimarães</p>
+  <Router>
+    <div>
+      <Link to="/">Sobre Mim</Link>
+      <Link to="/skills">Skills</Link>
+      <Link to="/projetos">Projetos</Link>
+      <Link to="/contatos">Contatos</Link>
     </div>
+    <Switch>
+      <Container>
+        <Route  exact path="/">
+          <Sobre />
+        </Route>
+        <Route  exact path="/skills">
+          <Skills />
+        </Route>  
+        <Route  exact path="/projetos">
+          <Projetos />
+        </Route>
+        <Route  exact path="/contatos">
+          <Contatos />
+        </Route>
+      </Container>  
+    
+    </Switch>
+    <p>Footer</p>
+  </Router>
   );
 }
 
